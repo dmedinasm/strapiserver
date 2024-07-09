72c4c4d375112eaa2e831b6927fbf9eb456ad015
Daniel Miguel Medina Mateo <38569766+dmedinasm@users.noreply.github.com>
2024-07-08 22:02:35 -0500

    Update middlewares to open strapi to localhost

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,17 +1,39 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-   {
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:', 'ws:', 'wss:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+          'frame-src': ["'self'", 'https:', 'http:'],
+        },
+      },
+      frameguard: false,
+      hsts: {
+        maxAge: 31536000,
+        includeSubDomains: true,
+        preload: true,
+      },
+      xssFilter: true,
+    },
+  },
+  {
     name: 'strapi::cors',
     config: {
       enabled: true,
       origin: ['*'],
       headers: ['*'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      credentials: true,
     }
   },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
